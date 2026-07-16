37d40e12d1d89384ae3991a134868da7ec52c675
Deepesh Hamal <deepeshhamal@Deepeshs-MacBook-Pro.local>
2026-07-16 16:00:48 +0545

    chore: limit Next.js build workers and concurrency for HostGator

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    staticGenerationMaxConcurrency: 1,
+    cpus: 1,
+  }
 };
 
 export default nextConfig;
