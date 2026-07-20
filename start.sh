24ea5a35fe9879004de2744aba13ea4be0515c40
Deepesh Hamal <deepeshhamal@Deepeshs-MacBook-Pro.local>
2026-07-20 09:59:41 +0545

    fix: add disown to background commands in start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ cd /home1/mjwgflte/public_html/website_875c4662/crm
 if ! nc -z 127.0.0.1 3004; then
   echo "Starting PGlite..."
   nohup node pglite-server.js > pglite.log 2>&1 &
+  disown
   sleep 3
 fi
 
@@ -13,4 +14,5 @@ fi
 if ! nc -z 127.0.0.1 3022; then
   echo "Starting Next.js..."
   nohup npx next start -p 3022 > next.log 2>&1 &
+  disown
 fi
